17cedca7c8a7cd8bbb8a6f71fbcd91a198b4d23a
Laniakea05 <163650800+Laniakea05@users.noreply.github.com>
2024-11-26 18:19:28 +0700

    Update cli.cpp

--- cli.cpp
+++ cli.cpp
@@ -8,16 +8,16 @@ using namespace std;
 
 int main() {
     // Создание сокета для клиента (IPv4, TCP)
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int cliSocket = socket(AF_INET, SOCK_STREAM, 0);
     
     // Определение структуры для адреса сервера
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET; // Указываем семью адресов (IPv4)
-    serverAddr.sin_port = htons(7432); // Указываем порт, к которому будем подключаться (7432)
-    serverAddr.sin_addr.s_addr = INADDR_ANY; // Указываем, что принимаем соединения на любом доступном интерфейсе
+    sockaddr_in servAdd;
+    servAdd.sin_family = AF_INET; // Указываем семью адресов (IPv4)
+    servAdd.sin_port = htons(7432); // Указываем порт, к которому будем подключаться (7432)
+    servAdd.sin_addr.s_addr = INADDR_ANY; // Указываем, что принимаем соединения на любом доступном интерфейсе
 
     // Подключение к серверу
-    connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    connect(cliSocket, (struct sockaddr*)&servAdd, sizeof(servAdd));
 
     string command; // Переменная для хранения введенной команды
     while (true) {
@@ -27,18 +27,18 @@ int main() {
         if (command.empty()) continue; // Если команда пустая, продолжаем цикл
 
         // Отправка команды на сервер
-        send(clientSocket, command.c_str(), command.size(), 0);
+        send(cliSocket, command.c_str(), command.size(), 0);
 
         // Если введена команда "EXIT", выходим из цикла
         if (command == "EXIT") break;
 
         char buffer[1024]; // Буфер для получения ответа от сервера
-        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0); // Получение данных от сервера
+        int bytesReceived = recv(cliSocket, buffer, sizeof(buffer), 0); // Получение данных от сервера
         if (bytesReceived > 0) { // Если данные были получены
             cout << "Server: " << string(buffer, bytesReceived) << endl; // Вывод ответа сервера
         }
     }
     
-    close(clientSocket); // Закрытие сокета клиента
+    close(cliSocket); // Закрытие сокета клиента
     return 0; // Завершение программы
 }
